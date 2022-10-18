49b31cf1000e27b5bd4ece161d553359d9868c78
kiran gautam <kirangautam225@gmail.com>
2022-10-18 14:55:38 +0545

    added

---
+++ backend/models/goalModel.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const goalSchema = mongoose.Schema(
+  {
+    text: {
+      type: String,
+      require: [true, "please add text value"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+module.exports=mongoose.model('Goal',goalSchema)
