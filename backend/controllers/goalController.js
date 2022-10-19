865cb42e00c872d6d1647be79da3e2d6ac6c64e3
kiran gautam <kirangautam225@gmail.com>
2022-10-19 07:29:49 +0545

    user auth added

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,36 +1,52 @@
+const { json } = require("express");
 const asyncHandler = require("express-async-handler");
 
-const  Goal= require("../models/goalModel")
+const Goal = require("../models/goalModel");
+const User = require("../models/userModel");
 //@desc Get goals
 // @route Get/api/goals
 //@access private
 const getGoals = asyncHandler(async (req, res) => {
-  const goals= await Goal.find()
-    res.status(200).json(goals)
-})
+  const goals = await Goal.find({ user: "634e8da6aeeec6d8ce18f7f1" });
+  const filter =  goals.filter((value) => { value.user, value.text })
+  res.json(filter)
+});
 //@desc Set goal
 // @route POST/api/goals
 //@access private
 const setGoal = asyncHandler(async (req, res) => {
   if (!req.body.text) {
-    res.status(400)
-    throw new Error('please add a text field')
+    res.status(400);
+    throw new Error("please add a text field");
   }
   const goal = await Goal.create({
-    text:req.body.text,
-  })
-  res.status(200).json({goal});
+    text: req.body.text,
+    user: req.user.id,
+  });
+  res.status(200).json({ goal });
 });
 //@desc update goals
 // @route PUT/api/goals/:id
 //@access private
 const updateGoal = asyncHandler(async (req, res) => {
-  const goal = await Goal.findById(req.params.id)
+  const goal = await Goal.findById(req.params.id);
   if (!goal) {
-    res.status(400)
-     throw new Error("goal not found");
+    res.status(400);
+    throw new Error("goal not found");
   }
-  const updatedGoal= await Goal.findByIdAndUpdate(req.params.id, req.body,{new:true})
+  const user = await User.findById(req.user.id);
+  //check for user
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+  if (goal.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
+  }
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
 
   res.status(200).json(updatedGoal);
 });
@@ -38,19 +54,28 @@ const updateGoal = asyncHandler(async (req, res) => {
 // @route DELETE/api/goals/:id
 //@access private
 const deleteGoal = asyncHandler(async (req, res) => {
-
-    const goal = await Goal.findById(req.params.id);
-    if (!goal) {
-      res.status(400);
-      throw new Error("goal not found");
-    }
-  await goal.remove()
-    res.status(200).json({ id: req.params.id});
+  const goal = await Goal.findById(req.params.id);
+  if (!goal) {
+    res.status(400);
+    throw new Error("goal not found");
+  }
+  const user = await User.findById(req.user.id);
+  //check for user
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+  if (goal.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
+  }
+  await goal.remove();
+  res.status(200).json({ id: req.params.id });
 });
 
 module.exports = {
-    getGoals,
-    setGoal,
-    updateGoal,
-    deleteGoal,
-}
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+};
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -33,6 +33,7 @@ const registerUser = asyncHandler(async (req, res) => {
       _id: user._id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
@@ -49,7 +50,8 @@ const loginUser = asyncHandler(async (req, res) => {
     res.json({
       _id: user._id,
       name: user.name,
-      email: user.email,
+        email: user.email,
+      token:generateToken(user._id)
     });
   } else {
     res.status(400);
@@ -61,8 +63,19 @@ const loginUser = asyncHandler(async (req, res) => {
 // @route GET /api/users/me
 //@access public
 const getMe = asyncHandler(async (req, res) => {
-  res.status(200).json(req.data);
+    const { _id, name, email } = await User.findById(req.user.id)
+    res.status(200).json({
+        id: _id,
+        name,
+        email,
+    })
 });
+//geneteate JWT
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+      expiresIn: "30d",
+    });
+}
 
 module.exports = {
   registerUser,
