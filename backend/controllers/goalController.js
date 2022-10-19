773159eff2e16438c5275798c71a672f43f6bc20
kiran gautam <kirangautam225@gmail.com>
2022-10-19 21:34:28 +0545

    delete goals

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,90 +1,95 @@
-const { json } = require("express");
-const asyncHandler = require("express-async-handler");
+const asyncHandler = require('express-async-handler')
 
-const Goal = require("../models/goalModel");
-const User = require("../models/userModel");
-//@desc Get goals
-// @route Get/api/goals
-//@access private
+const Goal = require('../models/goalModel')
+const User = require('../models/userModel')
+
+// @desc    Get goals
+// @route   GET /api/goals
+// @access  Private
 const getGoals = asyncHandler(async (req, res) => {
-  const goals = await Goal.find({ user: req.user.id });
+  const goals = await Goal.find({ user: req.user.id })
+
   res.status(200).json(goals)
+})
 
-  // const modifiedGoals = goals.map((goal) => {
-  //   const { _id, __v, text, ...rest } = goal.toObject();
-
-  //   return {
-  //     Description: text,
-  //     ...rest,
-  //   };
-  // });
-  // res.json(modifiedGoals);
-});
-//@desc Set goal
-// @route POST/api/goals
-//@access private
+// @desc    Set goal
+// @route   POST /api/goals
+// @access  Private
 const setGoal = asyncHandler(async (req, res) => {
   if (!req.body.text) {
-    res.status(400);
-    throw new Error("please add a text field");
+    res.status(400)
+    throw new Error('Please add a text field')
   }
+
   const goal = await Goal.create({
     text: req.body.text,
     user: req.user.id,
-  });
-  res.status(200).json({ goal });
-});
-//@desc update goals
-// @route PUT/api/goals/:id
-//@access private
+  })
+
+  res.status(200).json(goal)
+})
+
+// @desc    Update goal
+// @route   PUT /api/goals/:id
+// @access  Private
 const updateGoal = asyncHandler(async (req, res) => {
-  const goal = await Goal.findById(req.params.id);
+  const goal = await Goal.findById(req.params.id)
+
   if (!goal) {
-    res.status(400);
-    throw new Error("goal not found");
+    res.status(400)
+    throw new Error('Goal not found')
   }
-  const user = await User.findById(req.user.id);
-  //check for user
-  if (!user) {
-    res.status(401);
-    throw new Error("User not found");
+
+  // Check for user
+  if (!req.user) {
+    res.status(401)
+    throw new Error('User not found')
   }
-  if (goal.user.toString() !== user.id) {
-    res.status(401);
-    throw new Error("User not authorized");
+
+  // Make sure the logged in user matches the goal user
+  if (goal.user.toString() !== req.user.id) {
+    res.status(401)
+    throw new Error('User not authorized')
   }
+
   const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
-  });
+  })
 
-  res.status(200).json(updatedGoal);
-});
-//@desc update goals
-// @route DELETE/api/goals/:id
-//@access private
+  res.status(200).json(updatedGoal)
+})
+
+// @desc    Delete goal
+// @route   DELETE /api/goals/:id
+// @access  Private
 const deleteGoal = asyncHandler(async (req, res) => {
-  const goal = await Goal.findById(req.params.id);
+  const goal = await Goal.findById(req.params.id)
+
   if (!goal) {
-    res.status(400);
-    throw new Error("goal not found");
+    res.status(400)
+    throw new Error('Goal not found')
   }
-  const user = await User.findById(req.user.id);
-  //check for user
-  if (!user) {
-    res.status(401);
-    throw new Error("User not found");
+
+  // Check for user
+  if (!req.user) {
+    res.status(401)
+    throw new Error('User not found')
   }
-  if (goal.user.toString() !== user.id) {
-    res.status(401);
-    throw new Error("User not authorized");
+
+  // Make sure the logged in user matches the goal user
+  if (goal.user.toString() !== req.user.id) {
+    res.status(401)
+    throw new Error('User not authorized')
   }
-  await goal.remove();
-  res.status(200).json({ id: req.params.id });
-});
+
+  await goal.remove()
+
+  res.status(200).json({ id: req.params.id })
+})
 
 module.exports = {
   getGoals,
   setGoal,
   updateGoal,
   deleteGoal,
-};
+}
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,22 +1,24 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
+
 const userSchema = mongoose.Schema(
   {
     name: {
       type: String,
-      require: [true, "please add a name"],
+      required: [true, 'Please add a name'],
     },
     email: {
       type: String,
-      require: [true, "please add an email"],
+      required: [true, 'Please add an email'],
       unique: true,
     },
     password: {
       type: String,
-      require: [true, "please add a password"],
+      required: [true, 'Please add a password'],
     },
   },
   {
     timestamps: true,
   }
-);
-module.exports = mongoose.model("User", userSchema);
+)
+
+module.exports = mongoose.model('User', userSchema)
