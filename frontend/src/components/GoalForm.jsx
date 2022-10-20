a7eb43f94215ba4a4d40636c717747af4cb2e148
kiran gautam <kirangautam225@gmail.com>
2022-10-20 12:19:05 +0545

    info added

--- frontend/src/components/GoalForm.jsx
+++ frontend/src/components/GoalForm.jsx
@@ -18,7 +18,7 @@ function GoalForm() {
     <section className='form'>
       <form onSubmit={onSubmit}>
         <div className='form-group'>
-          <label htmlFor='text'>Goal</label>
+          <label htmlFor='text'>To Do Task</label>
           <input
             type='text'
             name='text'
@@ -29,7 +29,7 @@ function GoalForm() {
         </div>
         <div className='form-group'>
           <button className='btn btn-block' type='submit'>
-            Add Goal
+            Add To Do Task
           </button>
         </div>
       </form>
